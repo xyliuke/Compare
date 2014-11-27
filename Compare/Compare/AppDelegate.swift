cbf5bb7f5eb2e3069e1d77c506e051c90c516a08
liuke <xyliuke@gmail.com>
2014-11-27 17:09:18 +0800

    提交工程文件

---
+++ Compare/Compare/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Compare
+//
+//  Created by liuke on 14/11/27.
+//  Copyright (c) 2014年 liuke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
